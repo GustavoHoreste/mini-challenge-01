3367e7b51888cdf4ce0bba1b8f2490b9f993d55a
Thiago-Develooper <thiagoprogramador70x7@gmail.com>
2023-07-11 14:36:30 -0300

    Arquivo base.

---
+++ MiniChallenge01/Scenes/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  MiniChallenge01
+//
+//  Created by Thiago Pereira de Menezes on 11/07/23.
+//
+
+import UIKit
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+            self.view?.backgroundColor = .blue
+        
+        let bolinha:SKShapeNode = SKShapeNode(circleOfRadius: 25)
+        bolinha.fillColor = .purple
+        bolinha.strokeColor = .green
+        bolinha.lineWidth = 3
+        bolinha.position = CGPoint(x: 100, y: 100)
+        self.addChild(bolinha)
+        
+        }
+}
